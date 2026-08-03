550ea66857a815ebc64c23347aab306e7cc58502
Akshatha H M <127123535+AkshathaHM@users.noreply.github.com>
2026-08-03 13:26:20 +0530

    Time: 16 ms (20.99%) | Memory: 52.6 MB (85.63%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,22 @@
 class Solution {
-    // Moore Volting algorithm
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candi = 0;
+        int n = nums.length;
+         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num : nums) {
-            if(count == 0) {
-                candi = num;
-            }
-            if (candi == num) {
-                count++;
+         for(int i = 0; i < n; i++) {
+            if(map.containsKey(nums[i])) {
+                int count = map.get(nums[i]);
+                map.put(nums[i], count + 1);
             } else {
-                count--;
+                map.put(nums[i], 1);
             }
-        }
-        return candi;
+         }
+
+         for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if(entry.getValue() > n / 2) {
+                return entry.getKey();
+            }
+         }
+         return 0;
     }
 }
