e46c4f77cc3c9fecc791f026f5fd01a8ad01d65e
Akshatha H M <127123535+AkshathaHM@users.noreply.github.com>
2026-08-03 13:07:28 +0530

    Time: 2 ms (70.84%) | Memory: 55.5 MB (66.65%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    // Moore Volting algorithm
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candi = 0;
+
+        for(int num : nums) {
+            if(count == 0) {
+                candi = num;
+            }
+            if (candi == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candi;
+    }
+}
