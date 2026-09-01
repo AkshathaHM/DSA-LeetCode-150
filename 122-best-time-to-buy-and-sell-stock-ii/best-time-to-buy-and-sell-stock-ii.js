150edbcc813b4aa464333772f526935ae2ae8643
Akshatha H M <127123535+AkshathaHM@users.noreply.github.com>
2026-09-01 16:34:07 +0530

    Time: 1 ms (79.27%) | Memory: 54.1 MB (85.09%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let n = prices.length;
+    let profit = 0;
+
+    for(let i = 1; i < n; i++) {
+        if(prices[i] > prices[i-1]){
+            profit  += prices[i] - prices[i-1]
+        }
+    }
+    return profit;
+};
